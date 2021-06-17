5615824659fa6293589ed6777c932bb582027d3c
Dev Vrat <itsdvvs.18@gmail.com>
2021-06-18 00:13:21 +0530

    Implemented a theme controller for accessing and changing theme from anywhere

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
+import CSSBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
+import THEMES from "./themes/all_themes.js";
+import HomePage from "./pages/home.jsx";
+
+export const themeController = createContext();
 
 const App = () => {
-  return <h3>React App from Scratch!</h3>;
+  const [activeTheme, activateTheme] = useState(THEMES.paper);
+
+  useEffect(() => {
+    activateTheme(THEMES.paper);
+  }, []);
+
+  return (
+    <themeController.Provider value={{ activeTheme, activateTheme }}>
+      <MuiThemeProvider theme={createMuiTheme(activeTheme)}>
+        <CSSBaseline />
+        <HomePage />
+      </MuiThemeProvider>
+    </themeController.Provider>
+  );
 };
 
 export default App;
