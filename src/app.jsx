ac5707f6e280efd5f806fb53fa1a099294c9a83f
Dev Vrat <itsdvvs.18@gmail.com>
2021-06-18 16:39:35 +0530

    Site branding

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,31 @@
 import React, { createContext, useState, useEffect } from "react";
 import CSSBaseline from "@material-ui/core/CssBaseline";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
 
 import THEMES from "./themes/all_themes.js";
 import HomePage from "./pages/home.jsx";
 
 export const themeController = createContext();
 
+const useStyles = makeStyles((theme) => ({
+  gradientBackground: {
+    height: "100vh",
+    width: "100%",
+    background: `
+      linear-gradient(
+        180deg,
+        ${theme.palette.primary.main} 10%,
+        ${theme.palette.secondary.main} 100%
+      )`,
+  },
+}));
+
 const App = () => {
+  const style = useStyles();
   const [activeTheme, activateTheme] = useState(THEMES.paper);
 
   useEffect(() => {
@@ -18,7 +36,9 @@ const App = () => {
     <themeController.Provider value={{ activeTheme, activateTheme }}>
       <MuiThemeProvider theme={createMuiTheme(activeTheme)}>
         <CSSBaseline />
-        <HomePage />
+        <div className={style.gradientBackground}>
+          <HomePage />
+        </div>
       </MuiThemeProvider>
     </themeController.Provider>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,35 +1,41 @@
-import React, { useContext } from "react";
-import Button from "@material-ui/core/Button";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { themeController } from "../app.jsx";
-import THEME from "../themes/all_themes.js";
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    height: "100vh",
+  },
+  centered: {
+    width: "100%",
+    textAlign: "center",
+  },
+  horizontallyPadded: {
+    padding: "2% 30%",
+  },
+});
 
 const HomePage = () => {
-  const { activeTheme, activateTheme } = useContext(themeController);
-
-  function toggleTheme() {
-    console.log("Active Theme: ");
-    console.log(activeTheme);
-    if (activeTheme === THEME.dark) {
-      console.log("Changing to Paper theme...");
-      activateTheme(THEME.paper);
-    } else {
-      console.log("Changing to Dark theme...");
-      activateTheme(THEME.dark);
-    }
-  }
+  const style = useStyles();
+  const brandName = "BuJo";
+  const summary = `
+    Create a beautiful and fully functional journal all by yourself
+    without having any out-of-the-box "creative" skills.
+    We happily welcome you all to this world of digital journals.
+  `;
 
   return (
-    <React.Fragment>
-      <Typography variant="h2" color="textPrimary">
-        This is primary text
-      </Typography>
-      <Typography variant="h3" color="textSecondary">
-        This is secondary text
-      </Typography>
-      <Button onClick={toggleTheme}>Switch Theme</Button>
-    </React.Fragment>
+    <Grid container className={style.container}>
+      <Grid item xs={12} className={style.centered}>
+        <Typography variant="h1">{brandName}</Typography>
+        <Typography variant="subtitle1" className={style.horizontallyPadded}>
+          {summary}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
